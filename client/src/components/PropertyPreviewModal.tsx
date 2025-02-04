b62b4e0606323ebdc24d987d4a9e042748cc30b5
wesleyroos <wesleymathewroos@gmail.com>
2025-02-04 14:42:34 +0000

    Assistant checkpoint: Fix BrandingDialog trigger in PropertyPreviewModal
    Assistant generated file changes:
    - client/src/components/PropertyPreviewModal.tsx: Fix BrandingDialog trigger
    
    ---
    
    User prompt:
    
    Ok now when I click the export prevew button nothing is happening... but it should bring up the brandingcomponent

--- client/src/components/PropertyPreviewModal.tsx
+++ client/src/components/PropertyPreviewModal.tsx
@@ -576,9 +576,17 @@ export function PropertyPreviewModal({
               {property.title}
             </DialogTitle>
 
+            <BrandingDialog
+              open={showUpgradeModal}
+              onOpenChange={setShowUpgradeModal}
+              onGeneratePDF={(includeBranding) => {
+                generatePropertyPreviewPDF(property, includeBranding, user);
+              }}
+              onShowUpgrade={() => setShowUpgradeModal(true)}
+            />
             <Button 
               className="mr-6 bg-[#1BA3FF] hover:bg-[#1BA3FF]/90 text-white"
-              onClick={() => generatePropertyPreviewPDF(property, hasProAccess, user)}
+              onClick={() => setShowUpgradeModal(true)}
             >
               <FileText className="w-4 h-4 mr-2" />
               Export Preview
